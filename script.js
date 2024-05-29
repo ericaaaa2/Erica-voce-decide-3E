ee4495e0b12811469d5aa793fcd01939d2804b28
ericaaaa2 <163474434+ericaaaa2@users.noreply.github.com>
2024-05-29 23:46:45 +0000

    29/05/24

---
+++ script.js
@@ -0,0 +1,47 @@
+const caixaPrincipal = document.querySelector(".caixa-princaipal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+  {
+    enunciado: "pergunta 1",
+    alternativas: [
+      "resposta 1",
+      "resposta 2"
+    ]
+  },
+
+  {
+    enunciado: "pergunta 2",
+    alternativas: [
+      "resposta 1",
+      "resposta 2"
+    ]
+  },
+
+  {
+    enunciado: "pergunta 3",
+    alternativas: [
+      "resposta 1",
+      "resposta 2"
+    ]
+  },
+
+  {
+    enunciado: "pergunta 4",
+    alternativas: [
+      "resposta 1",
+      "resposta 2"
+    ]
+  },
+
+  {
+    enunciado: "pergunta 5",
+    alternativas: [
+      "resposta 1",
+      "resposta 2"
+    ]
+  }
+];
